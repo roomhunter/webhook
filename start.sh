d4a3b202294c583d41deb530f03b96ea16abe899
to0 <yuanancai@gmail.com>
2015-01-05 16:21:31 +0800

    log to logs dir

---
+++ start.sh
@@ -0,0 +1,11 @@
+if [ ! -d /srv/logs ]; then
+    mkdir /srv/logs
+fi
+
+if [ ! -d /srv/logs/webhook ]; then
+    mkdir /srv/logs/webhook
+    touch /srv/logs/webhook/out.log
+    touch /srv/logs/webhook/err.log
+fi
+
+forever start -a -o /srv/logs/webhook/out.log -e /srv/logs/webhook/err.log hook.js
