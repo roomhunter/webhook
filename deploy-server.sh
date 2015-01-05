ba7f293e9c4dfdb414bb515665c15fbb45b4506c
to0 <yuanancai@gmail.com>
2015-01-05 19:59:47 +0800

    actively start the server if not running

--- deploy-server.sh
+++ deploy-server.sh
@@ -2,3 +2,9 @@ cd /srv/
 
 # overwrite if exists already
 svn export --force https://github.com/roomhunter/server/trunk server
+
+cd roomhunter/server
+
+if ! forever restart index.js ; then
+    forever start -a -o --uid "server" /srv/logs/server/out.log -e /srv/logs/server/err.log hook.js
+fi
