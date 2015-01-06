47db9798659d81785dd6b3325b02460d6ed45664
to0 <yuanancai@gmail.com>
2015-01-06 20:42:45 +0800

    name err

--- deploy-server.sh
+++ deploy-server.sh
@@ -6,5 +6,5 @@ svn export --force https://github.com/roomhunter/server/trunk server
 cd server
 
 if ! forever restart index.js ; then
-    forever start -a --uid "server" -o /srv/logs/server/out.log -e /srv/logs/server/err.log hook.js
+    forever start -a --uid "server" -o /srv/logs/server/out.log -e /srv/logs/server/err.log index.js
 fi
