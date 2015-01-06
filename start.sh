30067aa08705208976b4e0d5d5dc790560813d7f
to0 <yuanancai@gmail.com>
2015-01-07 00:37:36 +0800

    add init scripts

--- start.sh
+++ start.sh
@@ -8,4 +8,7 @@ if [ ! -d /srv/logs/webhook ]; then
     touch /srv/logs/webhook/err.log
 fi
 
+cp services/roomhunter /etc/init.d/roomhunter
+ln -s /etc/init.d/roomhunter /etc/rc2.d/S90roomhunter
+
 forever start -a -w --uid "webhook" -o /srv/logs/webhook/out.log -e /srv/logs/webhook/err.log hook.js
