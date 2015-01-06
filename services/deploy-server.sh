bcfaf0d3b51e8746b78532fcb96d922386f0184d
to0 <yuanancai@gmail.com>
2015-01-07 00:22:34 +0800

    add npm install

--- services/deploy-server.sh
+++ services/deploy-server.sh
@@ -5,6 +5,8 @@ svn export --force https://github.com/roomhunter/server/trunk server
 
 cd server
 
+npm install
+
 if ! forever restart index.js ; then
     forever start -a --uid "server" -o /srv/logs/server/out.log -e /srv/logs/server/err.log index.js
 fi
