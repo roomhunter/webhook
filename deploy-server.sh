f6c1f19aad158d345df2ea45a23e6f19dc5c1a0b
Raymond Yuannan Cai <yuanancai@gmail.com>
2015-01-06 20:18:01 +0800

    Update deploy-server.sh

--- deploy-server.sh
+++ deploy-server.sh
@@ -6,5 +6,5 @@ svn export --force https://github.com/roomhunter/server/trunk server
 cd roomhunter/server
 
 if ! forever restart index.js ; then
-    forever start -a --uid -o "server" /srv/logs/server/out.log -e /srv/logs/server/err.log hook.js
+    forever start -a --uid "server" -o /srv/logs/server/out.log -e /srv/logs/server/err.log hook.js
 fi
