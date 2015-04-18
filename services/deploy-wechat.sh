ffe42a5aef6939c498cc99dd7d0dc3182072ac07
Daniel Yao <ly2328@columbia.edu>
2015-04-17 20:28:10 -0400

    Update deploy-wechat.sh

--- services/deploy-wechat.sh
+++ services/deploy-wechat.sh
@@ -6,5 +6,5 @@ npm update
 
 if ! forever restart ./bin/www ; then
     export NODE_ENV=production
-    forever start -a --uid "wechat-server" -o /srv/logs/wechat-server/out.log -e /srv/logs/wechat-server/err.log ./bin/www
+    forever start -a --uid "wechat-server" -o /srv/logs/wechat-server/out.log -e /srv/logs/wechat-server/err.log app.js
 fi
