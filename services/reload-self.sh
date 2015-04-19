f9b6bce207765b7dc7965b380ce75b3c76028363
to0 <yuanancai@gmail.com>
2015-04-19 01:19:42 -0400

    self deployment script

--- services/reload-self.sh
+++ services/reload-self.sh
@@ -1,2 +1,8 @@
 git pull
+
 npm install
+
+if ! forever restart hook.js ; then
+    export NODE_ENV=production
+    forever start -a -w --uid "webhook" -o /srv/logs/webhook/out.log -e /srv/logs/webhook/err.log hook.js
+fi
