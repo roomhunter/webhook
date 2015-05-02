adde148cbc36d009fd6c417fd5c69497bb943f48
Raymond Yuannan Cai <yuanancai@gmail.com>
2015-05-02 02:48:33 -0400

    Update deploy-homepage.sh

--- services/deploy-homepage.sh
+++ services/deploy-homepage.sh
@@ -8,6 +8,9 @@ fi
 
 cd /srv/web
 
+if [ ! -d homepage ]; then
+    mkdir homepage
+fi
 # overwrite if exists already
 rm -rf homepage
 svn export --force https://github.com/roomhunter/web-homepage/trunk/dist homepage
